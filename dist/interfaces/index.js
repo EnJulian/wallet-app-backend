62bb91af6b3862017b2dccff27ced6b29930d76b
Julian <36317786+EnJulian@users.noreply.github.com>
2024-02-09 13:03:51 +0000

    remove dist and ignore

--- dist/interfaces/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WalletType = exports.TransactionStatus = void 0;
-const transactionsDataType_1 = require("./transactionsDataType");
-Object.defineProperty(exports, "TransactionStatus", { enumerable: true, get: function () { return transactionsDataType_1.TransactionStatus; } });
-Object.defineProperty(exports, "WalletType", { enumerable: true, get: function () { return transactionsDataType_1.WalletType; } });
